7b721b2958dc48393ffa398cfe3c84e68ce1ab87
sahilnayab221 <snayab@unboundconnect.com>
2023-10-27 10:08:16 +0530

    there are 3 model of ml

---
+++ ML_model.py
@@ -0,0 +1,11 @@
+
+
+def model1():
+    print("this is my first model of ML")
+
+def model2():
+    print("this is my second model of ML")
+    
+
+ def model3():
+    print("this is my third model of ML")
